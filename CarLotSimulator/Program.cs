732cd836e0d0466fad257baa7c0c65d9ca1846ad
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-04-09 16:34:00 -0500

    Removed Car.cs

--- CarLotSimulator/Program.cs
+++ CarLotSimulator/Program.cs
@@ -11,7 +11,7 @@ namespace CarLotSimulator
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //The methods should take one string parameter: the respective noise property            
 
 
             //Now that the Car class is created we can instanciate 3 new cars
